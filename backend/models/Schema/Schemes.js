5158726be38b0f615e1b58b13b351e1bc8e6ea16
U Vivek Shenoy <vivekshenoy6763@gmail.com>
2025-06-02 15:21:35 +0530

    schemes db model

---
+++ backend/models/Schema/Schemes.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const schemeSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: { 
+    type: String,
+    required: true,
+  },
+  steps: [
+    {
+      content: {
+        type: String,
+      },
+      time: {
+        type: Date,
+        default: Date.now,
+      }
+    }
+  ],
+  details: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Official',
+    required: true,
+  },
+  application: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Application",
+  }
+});
+
+export default mongoose.model("Schemes", schemeSchema);
